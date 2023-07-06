ab1296b78b8592c532366166b7d28d34c8891382
Sejal Suryawanshi <126707897+sejalsuryawanshi@users.noreply.github.com>
2023-07-06 11:20:17 +0530

    Update Guessnumber.java

--- Task1-Number Guessing Game/Guessnumber.java
+++ Task1-Number Guessing Game/Guessnumber.java
@@ -1,1 +1,31 @@
-
+import java.util.Scanner;
+public class Guessnumber {
+    public static void guessnumber() {
+        Scanner sc = new Scanner(System.in);
+        int number = 1 +(int)(100*Math.random());
+        int k = 5;
+        int i,guess;
+        System.out.println("Guess the number between 1 to 100 within 5 trials");
+        for(i=0; i<k; i++) {
+            guess = sc.nextInt();
+        
+        if(guess == number) {
+            System.out.println("CONGRATULATIONS!! you guess the number");
+            break;
+        }
+        else if(guess>number && i != k-1) {
+            System.out.println("Your guess is greater than the number");
+        }
+        else if(guess<number && i != k-1) {
+            System.out.println("Your guess is less than the number");
+        }
+    }
+        if(i==k) {
+            System.out.println("You exhausted the trials");
+            System.out.println("The number was : "+number);
+        }
+    }
+    public static void main(String args[]) {
+        guessnumber();
+    }
+}
